7cfdc9f12b2a978e6bbe5a7b6a9637302b6753ed
ridhodori <ridhodori48@gmail.com>
2022-06-22 01:12:35 +0700

    fix

--- Procfile
+++ Procfile
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 web: bokeh serve --port=$PORT --allow-websocket-origin=tubes-visdat-web-based.herokuapp.com --address=0.0.0.0 --use-xheaders visdat1.py
-=======
-web: bokeh serve --show --port=$PORT --allow-websocket-origin=tubes-visdat-web-based-web.herokuapp.com --address=0.0.0.0 --use-xheaders myapp.py
->>>>>>> a7a2babe19bf3341deaab10e8500e05476fde37e
--- visdat1.py
+++ visdat1.py
@@ -1,108 +1,119 @@
-#!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
+# 
 
+from bokeh.io import output_notebook, curdoc
+from bokeh.plotting import figure
+from bokeh.models.widgets import Tabs, Panel
+from bokeh.models import ColumnDataSource, GroupFilter, CDSView, HoverTool, Div
+from bokeh.layouts import column, widgetbox
+import pandas as pd
+import numpy as np
 
+#import data
+df = pd.read_csv('./stock_market.csv', parse_dates=['Date'])
+df = df.rename(columns = {'Adj Close': 'Adj_Close'}, inplace = False)
+df.head()
 
+"""#Sortir data """
 
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-from bokeh.layouts import row, column, gridplot
-import pandas as pd  
-import os
-from bokeh.plotting import figure, ColumnDataSource
-from bokeh.models.widgets import Dropdown
-from bokeh.io import curdoc
-from bokeh.layouts import column
+#sort data
+source = ColumnDataSource(df)
+filter_hs = [GroupFilter(column_name='Name', group='HANG SENG')]
+source_hs = CDSView(source=source,filters=filter_hs)
 
-from bokeh.models import BooleanFilter, CDSView, Select, Range1d, HoverTool
-from bokeh.palettes import Category20
-from bokeh.models.formatters import NumeralTickFormatter
-from bokeh.io import output_notebook, show 
+filter_nk = [GroupFilter(column_name='Name', group='NIKKEI')]
+source_nk = CDSView(source=source,filters=filter_nk)
 
-# Define constants
-W_PLOT = 1500
-H_PLOT = 600
-TOOLS = 'pan,wheel_zoom,hover,reset'
+filter_ns = [GroupFilter(column_name='Name', group='NASDAQ')]
+source_ns = CDSView(source=source,filters=filter_ns)
 
-VBAR_WIDTH = 0.2
-RED = Category20[7][6]
-GREEN = Category20[5][4]
+#set circle info
+circle_data = {'source': source, 'size': 3, 'alpha': 0.7, 'selection_color':'black'}
 
-BLUE = Category20[3][0]
-BLUE_LIGHT = Category20[3][1]
+circle_hs = {'view': source_hs, 'color': 'red', 'legend_label': 'HANG SENG'}
 
-ORANGE = Category20[3][2]
-PURPLE = Category20[9][8]
-BROWN = Category20[11][10]
+circle_nk = {'view': source_nk, 'color': 'green', 'legend_label': 'NIKKEI'}
 
-def get_symbol_df(symbol=None):
-    df = pd.DataFrame(pd.read_csv('./GGRM.csv'))[-400:]
-    df.reset_index(inplace=True)
-    df["date"] = pd.to_datetime(df["date"])
-    return df
+circle_ns = {'view': source_ns, 'color': 'blue', 'legend_label': 'NASDAQ'}
 
+"""#Figure Adj_Close"""
 
-def plot_stock_price(stock):
-    p = figure(plot_width=W_PLOT, plot_height=H_PLOT, tools=TOOLS,
-               title="Stock price", toolbar_location='above')
+#create figur
+output_notebook()
+select_tools = ['pan', 'box_select', 'wheel_zoom', 'tap', 'reset']
+fig1 = figure(title= 'Adj Close Data',x_axis_type='datetime',x_axis_label='Date', y_axis_label= 'Adj Close',
+              plot_height=500, plot_width=800, toolbar_location="right",tools=select_tools)
 
-    inc = stock.data['close'] > stock.data['open_price']
-    dec = stock.data['open_price'] > stock.data['close']
-    view_inc = CDSView(source=stock, filters=[BooleanFilter(inc)])
-    view_dec = CDSView(source=stock, filters=[BooleanFilter(dec)])
+#add data circle
+fig1.circle(x='Date', y='Adj_Close', **circle_data, **circle_hs)
+fig1.circle(x='Date', y='Adj_Close', **circle_data, **circle_nk)
+fig1.circle(x='Date', y='Adj_Close', **circle_data, **circle_ns)
 
-    # map dataframe indices to date strings and use as label overrides
-    p.xaxis.major_label_overrides = {
-        i + int(stock.data['index'][0]): date.strftime('%b %d') for i, date in
-        enumerate(pd.to_datetime(stock.data["date"]))
-    }
-    p.xaxis.bounds = (stock.data['index'][0], stock.data['index'][-1])
+#add Hover
+tooltips= [ ('Name','@Name'),('Adj_Close', '@Adj_Close') ]
+hover_glyph = fig1.circle(x='Date', y= 'Adj_Close' , source=source,size=3, alpha=0, hover_fill_color='black', hover_alpha=0.5)
+fig1.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 
-    p.segment(x0='index', x1='index', y0='low', y1='high', color=RED, source=stock, view=view_inc)
-    p.segment(x0='index', x1='index', y0='low', y1='high', color=GREEN, source=stock, view=view_dec)
+#hide data via legend(Level 3)
+fig1.legend.click_policy = 'hide'
+fig1.legend.location= 'top_right'
 
-    p.vbar(x='index', width=VBAR_WIDTH, top='open_price', bottom='close', fill_color=BLUE, line_color=BLUE,
-           source=stock, view=view_inc, name="price")
-    p.vbar(x='index', width=VBAR_WIDTH, top='open_price', bottom='close', fill_color=RED, line_color=RED,
-           source=stock, view=view_dec, name="price")
+"""#Figure Volume (level2)"""
 
-    # p.legend.location = "top_left"
-    # p.legend.border_line_alpha = 0
-    # p.legend.background_fill_alpha = 0
-    # p.legend.click_policy = "mute"
+#create figur
+output_notebook()
+select_tools = ['pan', 'box_select', 'wheel_zoom', 'tap', 'reset']
+fig2 = figure(title= 'Volume Data',x_axis_type='datetime',x_axis_label='Date', y_axis_label= 'Volume',
+              plot_height=500, plot_width=800, toolbar_location="right",tools=select_tools)
 
-    p.yaxis.formatter = NumeralTickFormatter(format='$ 0,0[.]000')
-    p.x_range.range_padding = 0.05
-    p.xaxis.ticker.desired_num_ticks = 40
-    p.xaxis.major_label_orientation = 3.14 / 4
+#add data circle
+fig2.circle(x='Date', y='Volume', **circle_data, **circle_hs)
+fig2.circle(x='Date', y='Volume', **circle_data, **circle_nk)
+fig2.circle(x='Date', y='Volume', **circle_data, **circle_ns)
 
-    # Select specific tool for the plot
-    price_hover = p.select(dict(type=HoverTool))
+#add Hover
+tooltips= [ ('Name','@Name'),('Volume', '@Volume') ]
+hover_glyph = fig2.circle(x='Date', y= 'Volume' , source=source,size=3, alpha=0,hover_fill_color='black', hover_alpha=0.5)
+fig2.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 
-    # Choose, which glyphs are active by glyph name
-    price_hover.names = ["price"]
-    # Creating tooltips
-    price_hover.tooltips = [("datetime", "@date{%Y-%m-%d}"),
-                            ("open_price", "@open_price{$0,0.00}"),
-                            ("close", "@close{$0,0.00}"),
-                            ("volume", "@volume{($ 0.00 a)}")]
-    price_hover.formatters = {"date": 'datetime'}
+#hide data via legend(Level 3)
+fig2.legend.click_policy = 'hide'
+fig2.legend.location= 'top_right'
 
-    #return p
+"""#Figure Day_Perc_Change (level2)"""
 
-    show(p)
+#create figur
+output_notebook()
+select_tools = ['pan', 'box_select', 'wheel_zoom', 'tap', 'reset']
+fig3 = figure(title= 'Day_Perc_Change Data',x_axis_type='datetime',x_axis_label='Date', y_axis_label= 'Day_Perc_Change',
+              plot_height=500, plot_width=800, toolbar_location="right",tools=select_tools)
 
-stock = ColumnDataSource(
-    data=dict(Date=[], Open=[], Close=[], High=[], Low=[], index=[]))
-symbol = 'ggrm'
-df = get_symbol_df(symbol)
-stock.data = stock.from_df(df)
-elements = list()
+#add data circle
+fig3.circle(x='Date', y='Day_Perc_Change', **circle_data, **circle_hs)
+fig3.circle(x='Date', y='Day_Perc_Change', **circle_data, **circle_nk)
+fig3.circle(x='Date', y='Day_Perc_Change', **circle_data, **circle_ns)
 
-# update_plot()
-p_stock = plot_stock_price(stock)
+#add Hover
+tooltips= [ ('Name','@Name'),('Day_Perc_Change', '@Day_Perc_Change') ]
+hover_glyph = fig3.circle(x='Date', y= 'Day_Perc_Change' , source=source,size=3, alpha=0,hover_fill_color='black', hover_alpha=0.5)
+fig3.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 
+#hide data via legend(Level 3)
+fig3.legend.click_policy = 'hide'
+fig2.legend.location= 'top_right'
 
-curdoc().add_root(column(elements))
-curdoc().title = 'Bokeh stocks historical prices'
+"""#Configure Panel"""
 
+#add title
+isi = """<h1>Visualisasi Data Interaktif Fluktuasi Harga Saham</h1>
+<h3><i>Click Legend to HIDE Data</i><h3>"""
+title = Div(text=isi)
+#add widget panel and tab
+fig1_panel = Panel(child=fig1, title='Adj Close Data')
+fig2_panel = Panel(child=fig2, title='Volume Data')
+fig3_panel = Panel(child=fig3, title='Day_Perc_Change Data')
+tab = Tabs(tabs=[fig1_panel, fig2_panel, fig3_panel])
+#add layout
+layout = column(title,tab)
+curdoc().theme = 'dark_minimal'
+curdoc().add_root(layout)
