5851d0e61818f72d84ce36ab48a70d54d8d079d5
ridhodori <ridhodori48@gmail.com>
2022-06-22 01:45:51 +0700

    fix

--- visdat1.py
+++ visdat1.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# 
 
 from bokeh.io import output_notebook, curdoc
 from bokeh.plotting import figure
@@ -10,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 #import data
-df = pd.read_csv('./stock_market.csv', parse_dates=['Date'])
+df = pd.read_csv('./indexData.csv', parse_dates=['Date'])
 df = df.rename(columns = {'Adj Close': 'Adj_Close'}, inplace = False)
 df.head()
 
@@ -18,23 +17,23 @@ df.head()
 
 #sort data
 source = ColumnDataSource(df)
-filter_hs = [GroupFilter(column_name='Name', group='HANG SENG')]
+filter_hs = [GroupFilter(column_name='Index', group='NYA')]
 source_hs = CDSView(source=source,filters=filter_hs)
 
-filter_nk = [GroupFilter(column_name='Name', group='NIKKEI')]
+filter_nk = [GroupFilter(column_name='Index', group='IXIC')]
 source_nk = CDSView(source=source,filters=filter_nk)
 
-filter_ns = [GroupFilter(column_name='Name', group='NASDAQ')]
+filter_ns = [GroupFilter(column_name='Index', group='GDAXI')]
 source_ns = CDSView(source=source,filters=filter_ns)
 
 #set circle info
 circle_data = {'source': source, 'size': 3, 'alpha': 0.7, 'selection_color':'black'}
 
-circle_hs = {'view': source_hs, 'color': 'red', 'legend_label': 'HANG SENG'}
+circle_hs = {'view': source_hs, 'color': 'red', 'legend_label': 'NYA'}
 
-circle_nk = {'view': source_nk, 'color': 'green', 'legend_label': 'NIKKEI'}
+circle_nk = {'view': source_nk, 'color': 'green', 'legend_label': 'IXIC'}
 
-circle_ns = {'view': source_ns, 'color': 'blue', 'legend_label': 'NASDAQ'}
+circle_ns = {'view': source_ns, 'color': 'blue', 'legend_label': 'GDAXI'}
 
 """#Figure Adj_Close"""
 
@@ -50,7 +49,7 @@ fig1.circle(x='Date', y='Adj_Close', **circle_data, **circle_nk)
 fig1.circle(x='Date', y='Adj_Close', **circle_data, **circle_ns)
 
 #add Hover
-tooltips= [ ('Name','@Name'),('Adj_Close', '@Adj_Close') ]
+tooltips= [ ('Index','@Index'),('Adj_Close', '@Adj_Close') ]
 hover_glyph = fig1.circle(x='Date', y= 'Adj_Close' , source=source,size=3, alpha=0, hover_fill_color='black', hover_alpha=0.5)
 fig1.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 
@@ -58,7 +57,7 @@ fig1.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 fig1.legend.click_policy = 'hide'
 fig1.legend.location= 'top_right'
 
-"""#Figure Volume (level2)"""
+"""#Figure Volume"""
 
 #create figur
 output_notebook()
@@ -72,7 +71,7 @@ fig2.circle(x='Date', y='Volume', **circle_data, **circle_nk)
 fig2.circle(x='Date', y='Volume', **circle_data, **circle_ns)
 
 #add Hover
-tooltips= [ ('Name','@Name'),('Volume', '@Volume') ]
+tooltips= [ ('Index','@Index'),('Volume', '@Volume') ]
 hover_glyph = fig2.circle(x='Date', y= 'Volume' , source=source,size=3, alpha=0,hover_fill_color='black', hover_alpha=0.5)
 fig2.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 
@@ -80,22 +79,22 @@ fig2.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 fig2.legend.click_policy = 'hide'
 fig2.legend.location= 'top_right'
 
-"""#Figure Day_Perc_Change (level2)"""
 
+"""#Figure Open"""
 #create figur
 output_notebook()
 select_tools = ['pan', 'box_select', 'wheel_zoom', 'tap', 'reset']
-fig3 = figure(title= 'Day_Perc_Change Data',x_axis_type='datetime',x_axis_label='Date', y_axis_label= 'Day_Perc_Change',
+fig3 = figure(title= 'Open data',x_axis_type='datetime',x_axis_label='Date', y_axis_label= 'Open',
               plot_height=500, plot_width=800, toolbar_location="right",tools=select_tools)
 
 #add data circle
-fig3.circle(x='Date', y='Day_Perc_Change', **circle_data, **circle_hs)
-fig3.circle(x='Date', y='Day_Perc_Change', **circle_data, **circle_nk)
-fig3.circle(x='Date', y='Day_Perc_Change', **circle_data, **circle_ns)
+fig3.circle(x='Date', y='Open', **circle_data, **circle_hs)
+fig3.circle(x='Date', y='Open', **circle_data, **circle_nk)
+fig3.circle(x='Date', y='Open', **circle_data, **circle_ns)
 
 #add Hover
-tooltips= [ ('Name','@Name'),('Day_Perc_Change', '@Day_Perc_Change') ]
-hover_glyph = fig3.circle(x='Date', y= 'Day_Perc_Change' , source=source,size=3, alpha=0,hover_fill_color='black', hover_alpha=0.5)
+tooltips= [ ('Index','@Index'),('Open', '@Open') ]
+hover_glyph = fig3.circle(x='Date', y= 'Open' , source=source,size=3, alpha=0,hover_fill_color='black', hover_alpha=0.5)
 fig3.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_glyph]))
 
 #hide data via legend(Level 3)
@@ -111,7 +110,7 @@ title = Div(text=isi)
 #add widget panel and tab
 fig1_panel = Panel(child=fig1, title='Adj Close Data')
 fig2_panel = Panel(child=fig2, title='Volume Data')
-fig3_panel = Panel(child=fig3, title='Day_Perc_Change Data')
+fig3_panel = Panel(child=fig3, title='Open Data')
 tab = Tabs(tabs=[fig1_panel, fig2_panel, fig3_panel])
 #add layout
 layout = column(title,tab)
